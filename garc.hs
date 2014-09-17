690aed5c6d0dec382db1fbe65d163ec0a0129931
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2014-09-17 12:46:02 -0400

    Move lines16 up.
    This keeps all the pretty-printing sub-functions in the order they're
    first referenced.

--- garc.hs
+++ garc.hs
@@ -121,6 +121,13 @@ hexDump bytes = do
     (lineNum, line) <- lines16 bytes
     printf "%07X: %-39s  %s\n" lineNum (hexDumpBytes line) (hexDumpASCII line)
 
+-- Split a byte string into numbered sixteen-byte lines
+lines16 :: BL.ByteString -> [(Int, BL.ByteString)]
+lines16 = zip [0x0, 0x10..] . unfoldr line
+    where
+        line bytes | BL.null bytes = Nothing
+        line bytes = Just (BL.splitAt 16 bytes)
+
 -- Build the hex part of a hexdump line
 hexDumpBytes :: BL.ByteString -> String
 hexDumpBytes = intercalate " " . unfoldr col . (>>= printf "%02X") . BL.unpack
@@ -131,13 +138,6 @@ hexDumpBytes = intercalate " " . unfoldr col . (>>= printf "%02X") . BL.unpack
 -- Build the ASCII part of a hexdump line
 hexDumpASCII :: BL.ByteString -> String
 hexDumpASCII = map (\c -> if ' ' <= c && c <= '~' then c else '.') . BLC.unpack
-
--- Split a byte string into numbered sixteen-byte lines
-lines16 :: BL.ByteString -> [(Int, BL.ByteString)]
-lines16 = zip [0x0, 0x10..] . unfoldr line
-    where
-        line bytes | BL.null bytes = Nothing
-        line bytes = Just (BL.splitAt 16 bytes)
 
 
 -- From the command line: take filenames as args; read and pretty-print GARCs
