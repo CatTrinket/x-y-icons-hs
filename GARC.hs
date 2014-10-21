5413d59fbad716aa14f382611b4447a9c4b006f8
Lynn "Zhorken" Vaughan <zhorken@gmail.com>
2014-10-21 11:49:29 -0400

    Use some applicative style in GARC.

--- GARC.hs
+++ GARC.hs
@@ -1,6 +1,7 @@
 module GARC (GARC(..)) where
 
-import Control.Monad (liftM3, replicateM)
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (replicateM)
 import Data.Binary (Binary, Get, get, put)
 import Data.Binary.Get (bytesRead, getLazyByteString, getWord16le, getWord32le,
     lookAhead, skip)
@@ -10,8 +11,8 @@ import qualified Data.ByteString.Lazy.Char8 as BLC
 
 
 -- Include fromIntegral in Word-getters to avoid littering it everywhere
-getWord16le' = fmap fromIntegral getWord16le
-getWord32le' = fmap fromIntegral getWord32le
+getWord16le' = fromIntegral <$> getWord16le
+getWord32le' = fromIntegral <$> getWord32le
 
 -- GARC
 data GARC = GARC { files :: [[BL.ByteString]] } deriving (Show)
@@ -24,12 +25,12 @@ instance Binary GARC where
         fatbOffsets <- getFATO
         fileOffsets <- getFATB fatbOffsets
 
-        GARC `fmap` getFIMB dataOffset fileOffsets
+        GARC <$> getFIMB dataOffset fileOffsets
 
 -- GARC header
 getGARCHeader :: Get Int
 getGARCHeader = do
-    "CRAG" <- BLC.unpack `fmap` getLazyByteString 4  -- "Magic bytes"
+    "CRAG" <- BLC.unpack <$> getLazyByteString 4  -- "Magic bytes"
     0x0000001C <- getWord32le  -- Header length
     0xFEFF <- getWord16le  -- Byte order mark
     0x0400 <- getWord16le  -- Unknown
@@ -43,7 +44,7 @@ getGARCHeader = do
 -- FATO section
 getFATO :: Get [Int]
 getFATO = do
-    "OTAF" <- BLC.unpack `fmap` getLazyByteString 4  -- "Magic bytes"
+    "OTAF" <- BLC.unpack <$> getLazyByteString 4  -- "Magic bytes"
     skip 4  -- Length of this header + FATB offset list
     offsetCount <- getWord16le'  -- Number of FATB offsets
     0xFFFF <- getWord16le  -- Padding
@@ -53,7 +54,7 @@ getFATO = do
 -- FATB section
 getFATB :: [Int] -> Get [[(Int, Int, Int)]]
 getFATB offsets = do
-    "BTAF" <- BLC.unpack `fmap` getLazyByteString 4  -- "Magic bytes"
+    "BTAF" <- BLC.unpack <$> getLazyByteString 4  -- "Magic bytes"
     fatbLength <- getWord32le'
     skip 4  -- File count
 
@@ -69,17 +70,18 @@ getIndividualFATB offset = do
 
     -- Read 32-bit bitfield; get (start, end, length) for each bit set
     -- (Ignoring *which* bits are set — 011 and 101 are both just "two bits")
-    fileCount <- fmap popCount getWord32le'
-    replicateM fileCount (liftM3 (,,) getWord32le' getWord32le' getWord32le')
+    fileCount <- popCount <$> getWord32le'
+    replicateM fileCount $
+        (,,) <$> getWord32le' <*> getWord32le' <*> getWord32le'
 
 -- FIMB section
 getFIMB :: Int -> [[(Int, Int, Int)]] -> Get [[BL.ByteString]]
 getFIMB filesStart fileOffsets = do
-    "BMIF" <- BLC.unpack `fmap` getLazyByteString 4  -- "Magic bytes"
+    "BMIF" <- BLC.unpack <$> getLazyByteString 4  -- "Magic bytes"
     0x0000000C <- getWord32le  -- Header size
     skip 4  -- FIMB length
 
-    position <- fromIntegral `fmap` bytesRead
+    position <- fromIntegral <$> bytesRead
     skip (filesStart - position)
 
     mapM (mapM lookAhead) (map (map getIndividualFIMB) fileOffsets)
